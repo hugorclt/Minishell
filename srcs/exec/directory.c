7aec3df0bc658f42eaf32e07a6892c24e3ce6e63
Hugo Récolet <hugorecolet@gmail.com>
2022-04-17 16:50:28 +0200

    [UPDATE]function to get_pwd and print pwd

--- srcs/exec/directory.c
+++ srcs/exec/directory.c
@@ -6,20 +6,23 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 09:37:26 by hrecolet          #+#    #+#             */
-/*   Updated: 2022/04/16 09:39:45 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/04/17 16:11:41 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	get_pwd()
+void	print_pwd()
+{
+	printf("%s\n", get_pwd());
+}
+
+char	*get_pwd()
 {
 	char *buffer;
 
 	buffer = getcwd(NULL, 0);
 	if (!buffer)
-		return (1);
-	else
-		printf("%s\n", buffer);
-	return (0);
+		return (NULL);
+	return (buffer);
 }
