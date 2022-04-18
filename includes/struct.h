e475dc6f40857e1805b1b643765c7d91c875c045
Hugo RECOLET <hrecolet@e1r10p19.42.fr>
2022-04-18 12:26:03 +0200

    [Update]Struct added

--- includes/struct.h
+++ includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 11:51:52 by hrecolet          #+#    #+#             */
-/*   Updated: 2022/04/09 11:53:26 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/04/18 12:12:56 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,21 @@ typedef struct s_garb_col
 	void				*content;
 	struct s_garb_col	*next;
 }	t_garb;
+
+typedef struct s_cmd
+{
+	struct s_cmd	*next;
+	char			*cmd;
+	char			**path;
+	char			**option;
+	char			**args;
+}	t_cmd;
+
+typedef struct s_env
+{
+	char	actual_char;
+	int		nb_quotes;
+	int		nb_dquotes;
+}	t_env;
 
 #endif
