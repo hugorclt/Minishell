9430bcf167fe8b03daa23a2d50bf5d2e360c1fe0
Hugo Récolet <hugorecolet@gmail.com>
2022-04-24 11:10:51 +0200

    [FIX]fixed probable issues like cmd[i] segfault in ft_add_token

--- srcs/parsing/cmd_to_token.c
+++ srcs/parsing/cmd_to_token.c
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 16:53:42 by hrecolet          #+#    #+#             */
-/*   Updated: 2022/04/23 23:20:48 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/04/24 10:31:46 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,24 +29,16 @@ static void	ft_init_token(t_token **token)
 static void	ft_count_quotes(char c, t_token **token)
 {
 	if (c == '"')
-	{
 		(*token)->nb_dquotes++;
-		if ((*token)->nb_dquotes == 2)
-			(*token)->nb_dquotes = 0;
-	}
 	if (c == '\'')
-	{
 		(*token)->nb_quotes++;
-		if ((*token)->nb_quotes == 2)
-			(*token)->nb_quotes = 0;
-	}
 }
 
 static void	ft_add_token(t_list **lst, char *cmd, int i, t_token **token)
 {
 	ft_lstadd_back(lst, ft_lstnew(token));
 	ft_init_token(token);
-	if (cmd[i + 1] == cmd[i])
+	if (cmd[i + 1] && cmd[i + 1] == cmd[i])
 	{
 		(*token)->token = ft_strjoin_char((*token)->token, cmd[i]);
 		(*token)->token = ft_strjoin_char((*token)->token, cmd[i]);
@@ -71,8 +63,8 @@ void	ft_tokenization(t_list **lst, char *cmd)
 	while (cmd[i])
 	{
 		ft_count_quotes(cmd[i], &token);
-		if (is_operator(cmd[i]) && token->nb_dquotes == 0 &&
-			token->nb_quotes == 0)
+		if (is_operator(cmd[i]) && token->nb_dquotes % 2 == 0 &&
+			token->nb_quotes % 2 == 0)
 			ft_add_token(lst, cmd, i, &token);
 		ft_strjoin_char(token->token, cmd[i]);
 		i++;
