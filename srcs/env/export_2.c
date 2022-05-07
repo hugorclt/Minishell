020b94b3791a9fb34a2a8d45239aa02b49d87c48
Youkass <youribougre@gmail.com>
2022-05-07 15:25:43 +0200

    [FCK EXPORT] peut-etre finit

--- srcs/env/export_2.c
+++ srcs/env/export_2.c
@@ -6,7 +6,7 @@
 /*   By: yuro4ka <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 19:00:57 by yuro4ka           #+#    #+#             */
-/*   Updated: 2022/05/05 17:39:50 by yuro4ka          ###   ########.fr       */
+/*   Updated: 2022/05/07 15:23:46 by yuro4ka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	**ft_add_var(char *var, char **env)
 
 	size = ft_tab_size(env);
 	output = malloc(sizeof(char *) * (size + 2));
-	if (!output)
+	if (!output || !var)
 		return (NULL);
 	i = 0;
 	while (env[i])
@@ -77,4 +77,87 @@ char	**ft_change_var(char **env, char *var, int j)
 	}
 	output[i] = NULL;
 	return (ft_free(env), output);
+}
+
+static int	ft_check_equal(char *token)
+{
+	int	i;
+
+	i = 0;
+	while (token[i])
+	{
+		if (token[i] == '=')
+			return (1);
+		++i;
+	}
+	return (0);
+}
+
+static int	ft_theres_dquotes(char *token)
+{
+	int	i;
+
+	i = 0;
+	while (token[i])
+	{
+		if (token[i] == '"')
+			return (1);
+		++i;
+	}
+	return (0);
+}
+
+char	*ft_quote(char *token)
+{
+	int		i;
+	int		j;
+	char	*output
+
+	i = 0;
+	j = 0;
+	if (!ft_check_equal(token) || ft_theres_dquotes(token))
+		return (token);
+	output = malloc(sizeof(char) * (ft_strlen(token) + 3));
+	if (!output)
+		return (NULL);
+	while (token[i] != '=' && token[i])
+		output[j++] = token[i++];
+	output[j] = '"';
+	++j;
+	if (token[i])
+	{
+		while (token[i])
+			output[j++] = token[i++];
+	}
+	output[j] = '"';
+	output[j + 1] = 0;
+	return (output);
+}
+
+int	ft_export(t_token *t_token, char *token)
+{
+	char	**tmp;
+	int		i;
+
+	i = 1;
+	tmp = ft_split_space(token);
+	if (!tmp || !token)
+		return (-1);
+	if (ft_tab_size(tmp) < 2 && !ft_strcmp("export", tmp[0]))
+		return (ft_export_alph(t_token->env));
+	else
+	{
+		while (tmp[i])
+		{
+			if (ft_find_occ(tmp[i], t_token->env) == -1)
+				t_token->env = ft_add_var(ft_quote(tmp[i]), t_token->env);
+			else
+				t_token->env = ft_change_var(t_token->env, tmp[i], 
+					ft_find_occ(tmp[i], t_token->env));
+			if (!t_token->env)
+				return (ft_free_tab(tmp), -1);
+			++i;
+		}
+	}
+	return (ft_free_tab(tmp), 0);
 }
