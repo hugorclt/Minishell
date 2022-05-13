9f14fefa2b63a8cc41944bb9801fe83c855d2fe3
Youkass <youribougre@gmail.com>
2022-05-13 15:08:17 +0200

    [FIXED] unset.c

--- srcs/env/unset.c
+++ srcs/env/unset.c
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 16:13:48 by hrecolet          #+#    #+#             */
-/*   Updated: 2022/05/13 12:31:26 by yuro4ka          ###   ########.fr       */
+/*   Updated: 2022/05/13 15:07:39 by yuro4ka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_is_in(char **var_lst, char *var)
 	i = 1;
 	while (var_lst[i])
 	{
-		if (!ft_strncmp(var_lst[i], var, ft_strlen(var)))
+		if (!ft_strncmp(var_lst[i], var, ft_strlen(var_lst[i])))
 			return (1);
 		++i;
 	}
@@ -38,15 +38,22 @@ int	ft_need(char **env, char **var_lst)
 		while (env[j])
 		{
 			if (ft_strncmp(env[j], var_lst[i], ft_strlen(var_lst[i])) == 0)
-			{
-				printf("enculer\n");
 				return (1);
-			}
 			++j;
 		}
 		++i;
 	}
 	return (0);
+}
+
+static char	**ft_lil_malloc(int	size)
+{
+	char	**output;
+
+	output = malloc(sizeof(char *) * size); 
+	if (!output)
+		return (NULL);
+	return (output);
 }
 
 char	**ft_unset(char **env, char **var)
@@ -59,16 +66,13 @@ char	**ft_unset(char **env, char **var)
 	j = 0;
 	if (!ft_need(env, var))
 		return (env);
-	output = malloc(sizeof(char *) * (ft_tab_size(env) + 1 - (ft_tab_size(var) - 1)));
+	output = ft_lil_malloc(ft_tab_size(env) + 1 - (ft_tab_size(var) - 1));
 	if (!output)
 		return (NULL);
 	while (env[i])
 	{
 		if (ft_is_in(var, env[i]) == 1)
-		{
-			printf("je passe bien ici\n");
 			++i;
-		}
 		else
 		{
 			output[j] = ft_strdup(env[i]);
