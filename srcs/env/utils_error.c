5fbc75ec024a95bedb38b099ee2946c4d744aae1
Youkass <youribougre@gmail.com>
2022-05-20 16:31:15 +0200

    [FIXED] export and find_occurence

--- srcs/env/utils_error.c
+++ srcs/env/utils_error.c
@@ -6,27 +6,11 @@
 /*   By: yuro4ka <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 11:11:50 by yuro4ka           #+#    #+#             */
-/*   Updated: 2022/05/20 16:20:50 by yuro4ka          ###   ########.fr       */
+/*   Updated: 2022/05/20 16:30:32 by yuro4ka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-int	ft_strlen_exp(char *s)
-{
-	int	i;
-
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
-	{
-		if (s[i] == '=')
-			return (i);
-		++i;
-	}
-	return (i);
-}
 
 int	ft_strcmp_exp(char *s1, char *s2)
 {
@@ -35,7 +19,13 @@ int	ft_strcmp_exp(char *s1, char *s2)
 	i = 0;
 	if (!s1 || !s2)
 		return (-1);
-	while (s1[i] && s2[i] && (s1[i] == s2[i]) && s1[i] != '=' && s2[i] != '=')
+	while (s1[i] && s2[i] && (s1[i] == s2[i]))
+	{
+		if (s1[i + 1] == '=' || s2[i + 1] == '=')
+			break ;
 		++i;
+	}
 	return (s1[i] - s2[i]);
 }
+
+
