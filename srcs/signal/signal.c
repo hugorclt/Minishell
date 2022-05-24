a6ad273949c66dfe9e9f6bf6158bb01c20338ddd
Hugo RECOLET <hrecolet@e1r13p3.clusters.42paris.fr>
2022-05-25 01:31:23 +0200

    clean version

--- srcs/signal/signal.c
+++ srcs/signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 23:17:12 by yobougre          #+#    #+#             */
-/*   Updated: 2022/05/25 00:54:18 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/05/25 01:30:47 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 void	ft_sign_handle(int signo)
 {
 	(void)signo;
-	printf("test\n");
-	rl_replace_line("\n", 0);
 	write(1, "\nMinishell$>", 13);
 	g_last_status = 131;
 }
 
 void	sig_choice()
 {
-	signal(SIGQUIT, ft_sign_handle);
-	signal(SIGINT, SIG_IGN);
+	signal(SIGINT, ft_sign_handle);
+	signal(SIGQUIT, SIG_IGN);
 }
