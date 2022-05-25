ebfe00706461792ec54c7e1f5ca807cef557f8ce
Hugo RECOLET <hrecolet@e1r11p9.clusters.42paris.fr>
2022-05-25 20:20:39 +0200

    [clean version]

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 11:29:14 by hrecolet          #+#    #+#             */
-/*   Updated: 2022/05/25 18:36:10 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/05/25 20:19:14 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,6 +96,7 @@ static int	ft_refacto(t_node *params, t_list **lst, t_token *token, int flag)
 		}
 		add_history(cmd);
 		flag = ft_exec_parsing(token, cmd, params);
+		ft_print_tab(token->token);
 		if (flag == -1)
 			return (free(cmd), -1);
 		else if (flag == -2 || ft_check_token(token) == -1)
